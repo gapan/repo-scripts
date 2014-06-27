b76a6b89ae08d76e544c78b7c84ce0b7ecc8c644
George Vlahavas <vlahavas@gmail.com>
2014-06-27 16:20:38 +0300

    Add support for optional non-hidden scripts
    If non-hidden scripts are found, prefer those instead. Should help with
    testing.

--- sbo/sync.sh
+++ sbo/sync.sh
@@ -2,12 +2,21 @@
 
 VERSION="14.0"
 
-rsync -av --delete --exclude '.sync.sh' --exclude '.sb_dupes_req' \
+rsync -av --delete \
+	--exclude '.sync.sh' \
+	--exclude 'sync.sh' \
+	--exclude '.sb_dupes_req' \
+	--exclude 'sb_dupes_req' \
 	--exclude 'SBoEXCLUDE' \
 	rsync://slackbuilds.org/slackbuilds/$VERSION/ \
 	/var/www/vhosts/salix.enialis.net/pages/sbo/$VERSION
 
-./.sb_dupes_req
+# use the hidden file only when a non-hidden file is not there
+if [ -x ./sb_dupes_req ]; then
+	./sb_dupes_req
+else
+	./.sb_dupes_req
+fi
 mv SLACKBUILDS.TXT.NEW SLACKBUILDS.TXT
 cat SLACKBUILDS.TXT | gzip > SLACKBUILDS.TXT.gz
 
